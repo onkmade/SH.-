aa5729908b5fc186f50d24cf011d548363f6b31d
madesears <solankeadi11@gmail.com>
2025-10-29 21:35:08 +0530

    Initial JS

---
+++ Pages/JS/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const Login = document.getElementById('login-account');
+    const new_account = document.getElementById('new-account');
+    const signinBtn = document.getElementById('sign-in-btn');
+    const signupBtn = document.getElementById('sign-up-btn');
+
+    signinBtn.addEventListener('click', () => {
+        signinBtn.classList.add('')
+    });
+});
