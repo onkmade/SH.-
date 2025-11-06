a8cdf8b0ae1975133ad63a4697636dd82533ba1f
madesears <solankeadi11@gmail.com>
2025-11-06 20:04:03 +0530

    Complete application syntax, flask

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+# All flask start with an application instance
 
+from flask import Flask
+app = Flask(__name__) # Flask(capital F) is the class and the app is the application instance
+
+@app.route('/main')
+def main():
+    return '<h1>THIS IS MAIN PAGE</h1>'
